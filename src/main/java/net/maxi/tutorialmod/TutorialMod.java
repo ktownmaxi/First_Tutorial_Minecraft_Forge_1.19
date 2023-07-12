177384fabb1a13932327ea796800f3c048a98522
ktownmaxi <112746096+ktownmaxi@users.noreply.github.com>
2023-07-12 15:22:22 +0200

    Everything from the second tutorial added :)

--- src/main/java/net/maxi/tutorialmod/TutorialMod.java
+++ src/main/java/net/maxi/tutorialmod/TutorialMod.java
@@ -1,6 +1,7 @@
 package net.maxi.tutorialmod;
 
 import com.mojang.logging.LogUtils;
+import net.maxi.tutorialmod.item.ModItems;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -21,6 +22,7 @@ public class TutorialMod
     public TutorialMod()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        ModItems.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
---
+++ src/main/java/net/maxi/tutorialmod/item/ModItems.java
@@ -0,0 +1,23 @@
+package net.maxi.tutorialmod.item;
+
+import net.maxi.tutorialmod.TutorialMod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public  static  final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, TutorialMod.MOD_ID);
+
+    public static final RegistryObject<Item> ZIRCON = ITEMS.register("zircon",
+            () -> new Item(new Item.Properties().tab(ModCreativModeTab.TUTORIAL_TAB)));
+    public static final RegistryObject<Item> RAW_ZIRCON = ITEMS.register("raw_zircon",
+            () -> new Item(new Item.Properties().tab(ModCreativModeTab.TUTORIAL_TAB)));
+
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+
+}
