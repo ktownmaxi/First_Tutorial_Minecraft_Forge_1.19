79721cb02e4cfb8340d390dbe4f2bf5cd5182f55
ktownmaxi <112746096+ktownmaxi@users.noreply.github.com>
2023-07-15 15:50:21 +0200

    Everything added from the 5th Tutorial. Advanced Items added

--- src/main/java/net/maxi/tutorialmod/item/ModItems.java
+++ src/main/java/net/maxi/tutorialmod/item/ModItems.java
@@ -1,6 +1,7 @@
 package net.maxi.tutorialmod.item;
 
 import net.maxi.tutorialmod.TutorialMod;
+import net.maxi.tutorialmod.item.custom.EightBallItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -15,6 +16,8 @@ public class ModItems {
             () -> new Item(new Item.Properties().tab(ModCreativModeTab.TUTORIAL_TAB)));
     public static final RegistryObject<Item> RAW_ZIRCON = ITEMS.register("raw_zircon",
             () -> new Item(new Item.Properties().tab(ModCreativModeTab.TUTORIAL_TAB)));
+    public static final RegistryObject<Item> EIGHT_BALL = ITEMS.register("eight_ball",
+            () -> new EightBallItem(new Item.Properties().tab(ModCreativModeTab.TUTORIAL_TAB).stacksTo(1)));
 
     public static void register(IEventBus eventBus){
         ITEMS.register(eventBus);
