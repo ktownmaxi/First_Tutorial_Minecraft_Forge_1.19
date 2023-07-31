fafdcf5abf2d1a42e3b03d7698804b721d3bab78
ktownmaxi <112746096+ktownmaxi@users.noreply.github.com>
2023-07-31 09:24:25 +0200

    Hopper conections added

--- src/main/java/net/maxi/tutorialmod/block/entity/GemInfusingStationBlockEntity.java
+++ src/main/java/net/maxi/tutorialmod/block/entity/GemInfusingStationBlockEntity.java
@@ -1,9 +1,11 @@
 package net.maxi.tutorialmod.block.entity;
 
+import net.maxi.tutorialmod.block.custom.GemInfusingStationBlock;
 import net.maxi.tutorialmod.item.ModItems;
 import net.maxi.tutorialmod.recipe.GemInfusingStationRecipe;
 import net.maxi.tutorialmod.screen.GemInfusingStationMenu;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.Containers;
@@ -13,7 +15,7 @@ import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.ContainerData;
-import net.minecraft.world.item.Item;
+import net.minecraftforge.items.IItemHandler;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
@@ -27,22 +29,43 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.plaf.basic.BasicComboBoxUI;
+import java.util.Map;
 import java.util.Optional;
 
 public class GemInfusingStationBlockEntity extends BlockEntity implements MenuProvider {
-    private final ItemStackHandler ItemHandler = new ItemStackHandler(3) {
+    private final ItemStackHandler itemHandler = new ItemStackHandler(3) {
         @Override
         protected void onContentsChanged(int slot) {
             setChanged();
         }
+
+        @Override
+        public boolean isItemValid(int slot, @NotNull ItemStack stack) {
+            return switch (slot) {
+                case 0 -> stack.getItem() == ModItems.ZIRCON.get();
+                case 1 -> stack.getItem() == ModItems.RAW_ZIRCON.get();
+                case 2 -> false;
+                default -> super.isItemValid(slot, stack);
+            };
+        }
     };
 
     private LazyOptional<IItemHandler> lazyItemHandler = LazyOptional.empty();
+    private final Map<Direction, LazyOptional<WrappedHandler>> directionWrappedHandlerMap =
+            Map.of(Direction.DOWN, LazyOptional.of(() -> new WrappedHandler(itemHandler, (i) -> i == 2, (i, s) -> false)),
+                    Direction.NORTH, LazyOptional.of(() -> new WrappedHandler(itemHandler, (index) -> index == 1,
+                            (index, stack) -> itemHandler.isItemValid(1, stack))),
+                    Direction.SOUTH, LazyOptional.of(() -> new WrappedHandler(itemHandler, (i) -> i == 2, (i, s) -> false)),
+                    Direction.EAST, LazyOptional.of(() -> new WrappedHandler(itemHandler, (i) -> i == 1,
+                            (index, stack) -> itemHandler.isItemValid(1, stack))),
+                    Direction.WEST, LazyOptional.of(() -> new WrappedHandler(itemHandler, (index) -> index == 0 || index == 1,
+                            (index, stack) -> itemHandler.isItemValid(0, stack) || itemHandler.isItemValid(1, stack))));
+
+
 
     protected final ContainerData data;
     private int progress = 0;
     private int maxProgress = 78;
-
 
     public GemInfusingStationBlockEntity(BlockPos pos, BlockState state) {
         super(ModBlockEntities.GEM_INFUSING_STATION.get(), pos, state);
@@ -51,19 +74,22 @@ public class GemInfusingStationBlockEntity extends BlockEntity implements MenuPr
             public int get(int index) {
                 return switch (index) {
                     case 0 -> GemInfusingStationBlockEntity.this.progress;
-                    case  1 -> GemInfusingStationBlockEntity.this.maxProgress;
-                    default ->  0;
+                    case 1 -> GemInfusingStationBlockEntity.this.maxProgress;
+                    default -> 0;
                 };
             }
 
             @Override
-            public void set(int p_39285_, int p_39286_) {
-
+            public void set(int index, int value) {
+                switch (index) {
+                    case 0 -> GemInfusingStationBlockEntity.this.progress = value;
+                    case 1 -> GemInfusingStationBlockEntity.this.maxProgress = value;
+                }
             }
 
             @Override
             public int getCount() {
-                return 0;
+                return 2;
             }
         };
     }
@@ -80,28 +106,47 @@ public class GemInfusingStationBlockEntity extends BlockEntity implements MenuPr
     }
 
     @Override
-    public @NotNull <T> LazyOptional<T> getCapability(@NotNull Capability<T> cap) {
-        if (cap == ForgeCapabilities.ITEM_HANDLER) {  // Change not sure if working
-            return lazyItemHandler.cast();
+    public @NotNull <T> LazyOptional<T> getCapability(@NotNull Capability<T> cap, @Nullable Direction side) {
+        if (cap == ForgeCapabilities.ITEM_HANDLER) {
+            if(side == null) {
+                return lazyItemHandler.cast();
+            }
+
+            if(directionWrappedHandlerMap.containsKey(side)) {
+                Direction localDir = this.getBlockState().getValue(GemInfusingStationBlock.FACING);
+
+                if(side == Direction.UP || side == Direction.DOWN) {
+                    return directionWrappedHandlerMap.get(side).cast();
+                }
+
+                return switch (localDir) {
+                    default -> directionWrappedHandlerMap.get(side.getOpposite()).cast();
+                    case EAST -> directionWrappedHandlerMap.get(side.getClockWise()).cast();
+                    case SOUTH -> directionWrappedHandlerMap.get(side).cast();
+                    case WEST -> directionWrappedHandlerMap.get(side.getCounterClockWise()).cast();
+                };
+            }
         }
-        return super.getCapability(cap);
+
+        return super.getCapability(cap, side);
     }
 
     @Override
     public void onLoad() {
         super.onLoad();
-        lazyItemHandler = LazyOptional.of(() -> ItemHandler);
+        lazyItemHandler = LazyOptional.of(() -> itemHandler);
     }
 
     @Override
     public void invalidateCaps() {
+        super.invalidateCaps();
         lazyItemHandler.invalidate();
     }
 
     @Override
     protected void saveAdditional(CompoundTag nbt) {
-        nbt.put("inventory", ItemHandler.serializeNBT());
-        nbt.putInt("gem_infusing_station_progress", this.progress);
+        nbt.put("inventory", itemHandler.serializeNBT());
+        nbt.putInt("gem_infusing_station.progress", this.progress);
 
         super.saveAdditional(nbt);
     }
@@ -109,14 +154,14 @@ public class GemInfusingStationBlockEntity extends BlockEntity implements MenuPr
     @Override
     public void load(CompoundTag nbt) {
         super.load(nbt);
-        ItemHandler.deserializeNBT(nbt.getCompound("inventory"));
-        progress = nbt.getInt("gem_infusing_station_progress");
+        itemHandler.deserializeNBT(nbt.getCompound("inventory"));
+        progress = nbt.getInt("gem_infusing_station.progress");
     }
 
     public void drops() {
-        SimpleContainer inventory = new SimpleContainer(ItemHandler.getSlots());
-        for (int i = 0; i < ItemHandler.getSlots(); i++) {
-            inventory.setItem(i, ItemHandler.getStackInSlot(i));
+        SimpleContainer inventory = new SimpleContainer(itemHandler.getSlots());
+        for (int i = 0; i < itemHandler.getSlots(); i++) {
+            inventory.setItem(i, itemHandler.getStackInSlot(i));
         }
 
         Containers.dropContents(this.level, this.worldPosition, inventory);
@@ -142,51 +187,47 @@ public class GemInfusingStationBlockEntity extends BlockEntity implements MenuPr
 
     private void resetProgress() {
         this.progress = 0;
-        }
+    }
 
-    private static void craftItem(GemInfusingStationBlockEntity pEntity){
+    private static void craftItem(GemInfusingStationBlockEntity pEntity) {
         Level level = pEntity.level;
-        SimpleContainer inventory = new SimpleContainer(pEntity.ItemHandler.getSlots());
-        for (int i = 0; i < pEntity.ItemHandler.getSlots(); i++) {
-            inventory.setItem(i, pEntity.ItemHandler.getStackInSlot(i));
+        SimpleContainer inventory = new SimpleContainer(pEntity.itemHandler.getSlots());
+        for (int i = 0; i < pEntity.itemHandler.getSlots(); i++) {
+            inventory.setItem(i, pEntity.itemHandler.getStackInSlot(i));
         }
 
         Optional<GemInfusingStationRecipe> recipe = level.getRecipeManager()
                 .getRecipeFor(GemInfusingStationRecipe.Type.INSTANCE, inventory, level);
 
-        if (hasRecipe(pEntity)) {
-            pEntity.ItemHandler.extractItem(1, 1, false);
-            pEntity.ItemHandler.setStackInSlot(2, new ItemStack(recipe.get().getResultItem().getItem(),
-                    pEntity.ItemHandler.getStackInSlot(2).getCount() + 1 ));
+        if(hasRecipe(pEntity)) {
+            pEntity.itemHandler.extractItem(1, 1, false);
+            pEntity.itemHandler.setStackInSlot(2, new ItemStack(recipe.get().getResultItem().getItem(),
+                    pEntity.itemHandler.getStackInSlot(2).getCount() + 1));
 
             pEntity.resetProgress();
-
         }
     }
 
-    private static boolean hasRecipe(GemInfusingStationBlockEntity entity){
+    private static boolean hasRecipe(GemInfusingStationBlockEntity entity) {
         Level level = entity.level;
-        SimpleContainer inventory = new SimpleContainer(entity.ItemHandler.getSlots());
-        for (int i = 0; i < entity.ItemHandler.getSlots(); i++) {
-            inventory.setItem(i, entity.ItemHandler.getStackInSlot(i));
+        SimpleContainer inventory = new SimpleContainer(entity.itemHandler.getSlots());
+        for (int i = 0; i < entity.itemHandler.getSlots(); i++) {
+            inventory.setItem(i, entity.itemHandler.getStackInSlot(i));
         }
 
         Optional<GemInfusingStationRecipe> recipe = level.getRecipeManager()
                 .getRecipeFor(GemInfusingStationRecipe.Type.INSTANCE, inventory, level);
+
 
         return recipe.isPresent() && canInsertAmountIntoOutputSlot(inventory) &&
                 canInsertItemIntoOutputSlot(inventory, recipe.get().getResultItem());
     }
 
-    private static boolean canInsertItemIntoOutputSlot(SimpleContainer inventory, ItemStack itemStack) {
-
-        return inventory.getItem(2).getMaxStackSize() > inventory.getItem(2).getCount();
+    private static boolean canInsertItemIntoOutputSlot(SimpleContainer inventory, ItemStack stack) {
+        return inventory.getItem(2).getItem() == stack.getItem() || inventory.getItem(2).isEmpty();
     }
 
     private static boolean canInsertAmountIntoOutputSlot(SimpleContainer inventory) {
-
         return inventory.getItem(2).getMaxStackSize() > inventory.getItem(2).getCount();
     }
-
-
 }
