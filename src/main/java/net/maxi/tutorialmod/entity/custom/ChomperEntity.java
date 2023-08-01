ddaffe1b03e672234310645e27c774b8ed51c37f
ktownmaxi <112746096+ktownmaxi@users.noreply.github.com>
2023-08-01 11:30:25 +0200

    Chomper attack animation added

--- src/main/java/net/maxi/tutorialmod/entity/custom/ChomperEntity.java
+++ src/main/java/net/maxi/tutorialmod/entity/custom/ChomperEntity.java
@@ -19,6 +19,7 @@ import net.minecraft.world.entity.npc.AbstractVillager;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
+import software.bernie.geckolib3.core.AnimationState;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.PlayState;
 import software.bernie.geckolib3.core.builder.AnimationBuilder;
@@ -64,10 +65,21 @@ public class ChomperEntity extends Monster implements IAnimatable {
         return PlayState.CONTINUE;
     }
 
+    private PlayState attackPredicate(AnimationEvent event) {
+        if (this.swinging && event.getController().getAnimationState().equals(AnimationState.Stopped)) {
+            event.getController().markNeedsReload();
+            event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.chomper.attack", false));
+            return PlayState.CONTINUE;
+        }
+       return PlayState.CONTINUE;
+    }
+
     @Override
     public void registerControllers(AnimationData data) {
         data.addAnimationController(new AnimationController(this, "controller",
                 0, this::predicate));
+        data.addAnimationController(new AnimationController(this, "attackController",
+                0, this::attackPredicate));
     }
 
     @Override
